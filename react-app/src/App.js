d0467a2512f66566f08ddb7d7ad122523bb8bf7e
anakeeb <anakeeb@purdue.edu>
2020-05-28 22:17:46 -0400

    Implemented API to get stock data and parsed to find highest daily price

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,6 +6,39 @@ class App extends React.Component {
 	constructor() {
 		super()
 		this.state={
+			panels: [
+				{
+					id: 1,
+					name: 'AMZN',
+					clicked: false
+				},
+				{
+					id: 2,
+					name: 'GOOG',
+					clicked: false
+				},
+				{
+					id: 3,
+					name: 'NFLX',
+					clicked: false
+				},
+				{
+					id: 4,
+					name: 'MSFT',
+					clicked: false
+				},
+				{
+					id: 5,
+					name: 'AAPL',
+					clicked: false
+				},
+				// {
+				// 	id: 6,
+				// 	name: 'TSLA',
+				// 	clicked: false
+				// }
+				
+			]
 
 		}
 		this.handleClick = this.handleClick.bind(this)
@@ -13,17 +46,26 @@ class App extends React.Component {
 
 	handleClick(id) {
 		console.log(id)
+		this.setState(prevState => {
+			const updatedPanels = prevState.panels.map(panel => {
+				if (panel.id === id) {
+					panel.clicked = !(panel.clicked)
+				}
+				return panel
+			})
+			return {
+				panels: updatedPanels
+			}
+		})
+
+	
 	}
 
 	render() {
+		const stockPanels = this.state.panels.map(panel => <StockPanel key={ panel.id } panel={ panel } onClick={ this.handleClick }/>)
 		return (
 		  <div>
-		    <StockPanel id={1} onClick={ this.handleClick }/>
-		    <StockPanel id={2} onClick={ this.handleClick }/>
-		    <StockPanel id={3} onClick={ this.handleClick }/>
-		    <StockPanel id={4} onClick={ this.handleClick }/>
-		    <StockPanel id={5} onClick={ this.handleClick }/>
-		    <StockPanel id={6} onClick={ this.handleClick }/>
+		    { stockPanels }
 		  </div>
 		)
 	}
--- react-app/src/components/StockPanel.js
+++ react-app/src/components/StockPanel.js
@@ -3,14 +3,93 @@ import React from 'react'
 class StockPanel extends React.Component {
 	constructor() {
 		super()
-		this.state={}
+		var d = new Date()
+		let day = d.getDate()
+		if (day < 10) {
+			day = '0' + day.toString()
+		}
+		else {
+			day = day.toString()
+		}
+		let month = d.getMonth() + 1
+		if (month < 10) {
+			month = '0' + month.toString()
+		}
+		else {
+			month = month.toString()
+		}
+		let year = d.getFullYear()
+		year = year.toString()
+		this.state={
+			timeSeries: null,
+			date: `${year}-${month}-${day} `
+		}
+		this.findHighestDailyPrice = this.findHighestDailyPrice.bind(this)
 	}
 
+
+	findHighestDailyPrice() {
+		if (this.state.timeSeries === null) {
+			console.log('returning')
+			return 0
+		}
+		let highest = 0
+		console.log(this.state.timeSeries)
+		for (let i = 10; i < 17; i++) {
+			for(let j = 0; j < 56; j += 5) {
+				if ((i === 16) && (j > 0)) {
+					return highest
+				}
+				if (j < 10) {
+					console.log(this.state.date + `${i}:0${j}:00`)
+					console.log(this.state.timeSeries[this.state.date + `${i}:0${j}:00`])
+					console.log(this.state.timeSeries[this.state.date + `${i}:0${j}:00`]['2. high'])
+					let high = parseInt(this.state.timeSeries[this.state.date + `${i}:0${j}:00`]['2. high'], 10)
+					if (high >= highest) {
+						highest = high
+					}
+				}
+				else {
+					console.log(this.state.date + `${i}:${j}:00`)
+					console.log(this.state.timeSeries[this.state.date + `${i}:${j}:00`]['2. high'])
+					console.log(this.state.timeSeries[this.state.date + `${i}:${j}:00`]['2. high'])
+					let high = parseInt(this.state.timeSeries[this.state.date + `${i}:${j}:00`]['2. high'], 10)
+					if (high >= highest) {
+						highest = high
+					}
+				}
+				
+			}
+		}
+		console.log(highest)
+		return highest
+	}
+
+	componentDidMount() {
+        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ this.props.panel.name }&interval=5min&apikey=PUITOEHB47EXYOC4"`)
+            .then(response => response.json())
+            .then(data => this.setState(prevState => {
+            	return {
+            		timeSeries: data['Time Series (5min)'],
+                	date: prevState.date
+            	}
+            }))
+    }
+
 	render() {
+		console.log(this.props.panel.name)
+		console.log(this.state.timeSeries)
+		console.log(this.state.date)
+		
+		let highest = this.findHighestDailyPrice()
+
+
 		return(
 			<div>
-				<h1>{this.props.id}</h1>
-				<button onClick={ () => this.props.onClick(this.props.id) }>stock</button>
+				<h1> <hr/> { this.props.panel.name }</h1>
+				<h1>{ highest }</h1>
+				<h1>{ this.props.panel.clicked ? 'clicked' : 'not clicked' }</h1>
+				<button onClick={ () => this.props.onClick(this.props.panel.id) }>stock</button>
 			</div>
 		)
 	}
