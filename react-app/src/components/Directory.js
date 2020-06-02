38cb7c84cddf11d4222f9ee8a979c854eb270d81
anakeeb <anakeeb@purdue.edu>
2020-06-02 11:34:37 -0400

    added directory

--- react-app/src/components/Directory.js
+++ react-app/src/components/Directory.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+
 
 class Directory extends React.Component {
 	constructor() {
@@ -9,15 +14,31 @@ class Directory extends React.Component {
 		}
 
 	}
-
 	
 	render() {
 		return(
-
-			<div >
-				<button onClick={ this.props.handleHomeClick }>home</button>
-				<button onClick={ this.props.handleAboutClick }>about</button>
-			</div>
+			<>
+			    <Navbar bg="light" variant="light">
+				    <Navbar.Brand href="#home">
+				    	<img
+					        src="./logo.svg"
+					        width="30"
+					        height="30"
+					        className="d-inline-block align-top"
+					        alt="Financial Forecast logo"
+					    />
+				    </Navbar.Brand>
+				    <Nav className="mr-auto">
+				        <Nav.Link href="#home">Home</Nav.Link>
+				        <Nav.Link href="#features">Features</Nav.Link>
+				        <Nav.Link href="#pricing">Pricing</Nav.Link>
+				    </Nav>
+				    <Form inline>
+				        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+				        <Button variant="outline-info">Search</Button>
+				    </Form>
+				</Navbar>
+			</>
 		)
 	}
 }
